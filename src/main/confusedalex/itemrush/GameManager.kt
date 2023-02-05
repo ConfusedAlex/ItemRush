c992477a373b7f58245db5d88b133089b945b697
alex <alex@epostnet.de>
2023-02-05 16:08:52 +0100

    Overhauled Game States

--- src/main/confusedalex/itemrush/GameManager.kt
+++ src/main/confusedalex/itemrush/GameManager.kt
@@ -1,24 +1,26 @@
 package itemrush
 
-import itemrushfirst.Scoreboard
+import itemrush.GameStates.LOBBY
+import itemrush.GameStates.RUNNING
 import net.md_5.bungee.api.ChatColor.GOLD
-import org.bukkit.*
-import org.bukkit.entity.Boss
+import org.bukkit.Bukkit
+import org.bukkit.Sound
 import org.bukkit.scheduler.BukkitRunnable
 import java.util.*
-import java.util.logging.Level
+
 
 class GameManager (val itemRush: ItemRush) {
+    var gameState = LOBBY
     var pointMap = HashMap<UUID, Int?>()
     val itemMap: HashMap<UUID, ArrayList<String>> = HashMap()
     val scoreboard: Scoreboard = Scoreboard(this)
-    val bossbar = Bossbar(this)
+    private val bossbar = Bossbar(this)
     var running = false
-    private val seeds = listOf(42259133905528715, 3088695298922539727, -856662231300496405)
+    private val seeds = listOf(42259133905528715, 3088695298922539727, -856662231300496405, -3864653749869810919)
 
 
     fun getPlayerPointsByScoreboardPlace (i: Int) : Int? {
-        val sortedPointMap = pointMap.entries.sortedBy { it.value }.associate { it.toPair() }.toList()
+        val sortedPointMap = pointMap.entries.sortedByDescending { it.value }.associate { it.toPair() }.toList()
         val size = sortedPointMap.size - 1
 
         println(sortedPointMap)
@@ -31,7 +33,7 @@ class GameManager (val itemRush: ItemRush) {
     }
 
     fun getPlayerNameByScoreboardPlace (i: Int) : String? {
-        val sortedPointMap = pointMap.entries.sortedBy { it.value }.associate { it.toPair() }.toList()
+        val sortedPointMap = pointMap.entries.sortedByDescending { it.value }.associate { it.toPair() }.toList()
         val size = sortedPointMap.size - 1
 
         println(sortedPointMap)
@@ -44,64 +46,60 @@ class GameManager (val itemRush: ItemRush) {
     }
 
     fun switchGameState(gameStates: GameStates) {
-        if (gameStates == GameStates.STARTING) {
-            val wc = WorldCreator(Random().nextInt().toString())
-            val mapSeed = seeds[Random().nextInt(1, seeds.size)]
-            wc.seed(mapSeed)
-            wc.environment(World.Environment.NORMAL)
-            wc.type(WorldType.NORMAL)
+        gameState = gameStates
 
-            val world = wc.createWorld()
-            if (world == null) {
-                Bukkit.getLogger().log(Level.SEVERE,"World is null")
-                return
+        when (gameStates) {
+            LOBBY -> {
+
             }
 
-            world.worldBorder.setSize(200.0, 0)
-            world.time = 6000
-            world.setGameRule(GameRule.DO_DAYLIGHT_CYCLE, false)
+            GameStates.STARTING -> {
+                for (player in Bukkit.getOnlinePlayers()) {
+                    itemRush.world.let { player.teleport(it.spawnLocation) }
+                    player.inventory.clear()
+                    pointMap[player.uniqueId] = 0
+                    itemMap[player.uniqueId] = ArrayList<String>()
 
-            for (player in Bukkit.getOnlinePlayers()) {
-                player.teleport(world.spawnLocation)
-                player.inventory.clear()
-                pointMap[player.uniqueId] = 0
-                itemMap[player.uniqueId] = ArrayList<String>()
-
-                scoreboard.setScoreboard(player)
-            }
-            val bukkitRunnable = object: BukkitRunnable(){
-                var seconds = 10
-                override fun run() {
-                    if (seconds == 0) {
-                        running = true
-                        switchGameState(GameStates.RUNNING)
-                        cancel()
-                        return
-                    }
-
-                    for (player in Bukkit.getOnlinePlayers()) {
-                        player.sendTitle(seconds.toString(),  "", 5, 10, 5)
-                    }
-
-                    seconds--
+                    scoreboard.setScoreboard(player)
                 }
+
+                val bukkitRunnable = object: BukkitRunnable(){
+                    var seconds = 10
+                    override fun run() {
+                        if (seconds == 0) {
+                            running = true
+                            switchGameState(RUNNING)
+                            cancel()
+                            return
+                        }
+
+                        for (player in Bukkit.getOnlinePlayers()) {
+                            player.sendTitle(seconds.toString(),  "", 5, 10, 5)
+                            player.playSound(player.location, Sound.BLOCK_NOTE_BLOCK_PLING, 1.0f, 1.0f)
+                        }
+
+                        seconds--
+                    }
+                }
+                bukkitRunnable.runTaskTimer(itemRush, 0, 20)
+
             }
-            bukkitRunnable.runTaskTimer(itemRush, 0, 20)
+            RUNNING -> {
+                for (player in Bukkit.getOnlinePlayers()) {
+                    player.sendTitle("Sammel so viele Items wie du kannst!",  "Lauf Pirat, Lauf!", 5, 30, 5)
+                }
+                bossbar.createBossBar(30)
 
-        }
-
-        if (gameStates == GameStates.RUNNING) {
-            for (player in Bukkit.getOnlinePlayers()) {
-                player.sendTitle("Sammel so viele Items wie du kannst!",  "Lauf Pirat, Lauf!", 5, 30, 5)
             }
-            bossbar.createBossBar(5*60)
 
-        }
+            GameStates.END -> {
+                for (player in Bukkit.getOnlinePlayers()) {
+                    player.sendTitle("$GOLD 1. ${getPlayerNameByScoreboardPlace(1)}",
+                        "$GOLD 2. ${getPlayerNameByScoreboardPlace(2)} 3. ${getPlayerNameByScoreboardPlace(3)}", 10, 200, 10)
+                }
 
-        if (gameStates == GameStates.END) {
-            for (player in Bukkit.getOnlinePlayers()) {
-                player.sendTitle("$GOLD 1. ${getPlayerNameByScoreboardPlace(3)}",
-                    "$GOLD 2. ${getPlayerNameByScoreboardPlace(2)} 3. ${getPlayerNameByScoreboardPlace(1)}", 10, 200, 10)
+                Bukkit.getServer().shutdown()
+
             }
         }
 
