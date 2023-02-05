7b132a946903478f7aad6e3c9e673cf4fb421319
alex <alex@epostnet.de>
2023-02-05 16:09:03 +0100

    Implemented World copying

--- src/main/confusedalex/itemrush/ItemRush.kt
+++ src/main/confusedalex/itemrush/ItemRush.kt
@@ -1,16 +1,43 @@
 package itemrush
 
 import org.bukkit.Bukkit
+import org.bukkit.GameRule
+import org.bukkit.World
 import org.bukkit.WorldCreator
 import org.bukkit.plugin.java.JavaPlugin
+import java.io.File
+
 
 class ItemRush : JavaPlugin() {
     val gameManager = GameManager(this)
+    lateinit var world: World
 
     @Override
     override fun onEnable() {
         Bukkit.getPluginManager().registerEvents(Events(this), this)
         this.getCommand("itemrush")?.setExecutor(Commands(this))
+
+//        Bukkit.getServer().createWorld(WorldCreator("1"))
+//        val source = Bukkit.getWorld("1")
+        val sourceFolder = File("./1")
+
+        val targetFolder = File("./1_temp")
+        targetFolder.deleteRecursively()
+        targetFolder.delete()
+        targetFolder.mkdir()
+
+
+        sourceFolder.copyRecursively(targetFolder, true)
+
+        Bukkit.getServer().createWorld(WorldCreator("1_temp"))
+
+        world = Bukkit.getWorld("1_temp")!!
+
+        world?.worldBorder?.setSize(200.0, 0)
+        world?.time = 6000
+        world?.setGameRule(GameRule.DO_DAYLIGHT_CYCLE, false)
+
+        println(Bukkit.getWorlds().toString())
     }
 
     @Override
